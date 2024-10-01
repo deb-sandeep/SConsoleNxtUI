6822a1a1287f3c950035cec75ccb8e8e39d57779
Sandeep Deb <deb.sandeep@gmail.com>
2024-10-01 10:25:00 +0530

    New app - memgym::arithmetic introduced with routing

--- projects/app-memgym-arithmetic/src/app/app.component.ts
+++ projects/app-memgym-arithmetic/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common' ;
 import { PageHeaderComponent } from 'lib-core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ CommonModule, PageHeaderComponent ],
+  imports: [ CommonModule, PageHeaderComponent, RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
