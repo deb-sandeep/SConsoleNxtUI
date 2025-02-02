0bd4c9a0447c5aa8dcbd2eb2b738b058970c81f3
Sandeep Deb <deb.sandeep@gmail.com>
2025-02-02 11:24:09 +0530

    Topic chapter mapping - server integration complete

--- projects/app-jee-mdm/src/features/manage-problems/manage-problems.service.ts
+++ projects/app-jee-mdm/src/features/manage-problems/manage-problems.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
-import { APIResponse, RemoteService } from "lib-core";
+import { RemoteService } from "lib-core";
 
 import { environment } from "../../../../environments/environment";
 import { TopicChapterMapping } from "./manage-problems.type";
-
-import { topicChapterListTestdata } from "./test-data/topic-chapter-list.testdata";
 
 @Injectable()
 export class ManageProblemsService extends RemoteService {
 
   getTopicChapterMappings( syllabusName:string ):Promise<TopicChapterMapping[]> {
-    return new Promise<TopicChapterMapping[]>( (resolve, reject) => {
-      resolve( topicChapterListTestdata ) ;
-    }) ;
+    const url:string = `${environment.apiRoot}/Master/Topic/ChapterMappings?syllabusName=${syllabusName}` ;
+    return this.getPromise( url, true ) ;
   }
 }
