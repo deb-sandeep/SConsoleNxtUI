5d7ff95a68ef5697d352dad6fdf2f298f7c3a104
Sandeep Deb <deb.sandeep@gmail.com>
2026-04-03 20:34:48 +0530

    Making the answer zone runtime pluggable

---
+++ projects/app-jee-exam/src/features/jee-main/pages/04-exam-screen/question-display/sca-answer-zone/sca-answer-zone.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sca-answer-zone',
+  imports: [],
+  templateUrl: './sca-answer-zone.component.html',
+  styleUrl: './sca-answer-zone.component.css'
+})
+export class ScaAnswerZoneComponent {
+
+}
