28e6ea70dc733702ed8088af4189068a47e9fd15
Sandeep Deb <deb.sandeep@gmail.com>
2026-07-14 13:19:56 +0530

    Jee advanced CBT iterations in progress

--- projects/app-jee-exam/src/features/jee-advanced/pages/04-exam-screen/exam-screen.component.ts
+++ projects/app-jee-exam/src/features/jee-advanced/pages/04-exam-screen/exam-screen.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'exam-screen',
-  template: `I am here`
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './exam-screen.component.html',
+  styleUrl: './exam-screen.component.css'
 })
 export class ExamScreenComponent {
 }
