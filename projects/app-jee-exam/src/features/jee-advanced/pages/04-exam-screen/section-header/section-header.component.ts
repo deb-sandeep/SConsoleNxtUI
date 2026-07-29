010cc8a46105caa5ba635211c94badfc20b60b53
Sandeep Deb <deb.sandeep@gmail.com>
2026-07-29 23:55:21 +0530

    Sections are now displaying dynamically.

--- projects/app-jee-exam/src/features/jee-advanced/pages/04-exam-screen/section-header/section-header.component.ts
+++ projects/app-jee-exam/src/features/jee-advanced/pages/04-exam-screen/section-header/section-header.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { JeeAdvancedService } from "../../../jee-advanced.service";
 import { ExamApiService } from "@jee-common/services/exam-api.service";
 import { EventLogService } from "@jee-common/services/event-log.service";
+import { ExamSection } from "../../../../../common/so-wrappers";
 
 @Component({
   selector: 'section-header',
