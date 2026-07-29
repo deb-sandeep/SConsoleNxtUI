591d98d3d8811791a00ef0db34f416cd89c45f6e
Sandeep Deb <deb.sandeep@gmail.com>
2026-07-29 13:12:39 +0530

    Paper title now picks up year and paper number dynamically

--- projects/app-jee-exam/src/features/jee-advanced/jee-advanced.service.ts
+++ projects/app-jee-exam/src/features/jee-advanced/jee-advanced.service.ts
@@ -8,4 +8,21 @@ export class JeeAdvancedService extends JeeBaseService {
     this.examConfig = await this.apiSvc.getExamDetails( examId ) ;
     this.timeLeftInSeconds.set( this.examConfig.duration ) ;
   }
+
+  getPaperTitle(): string {
+    if( !this.examConfig ) {
+      return 'JEE Advanced' ;
+    }
+    const paperText = this.getPaperText( this.examConfig.note ) ;
+    return `JEE Advanced ${this.getExamYear()}${ paperText ? ' ' + paperText : '' }` ;
+  }
+
+  private getExamYear(): number {
+    return new Date() < new Date( 2027, 4, 1 ) ? 2027 : 2028 ;
+  }
+
+  private getPaperText( note: string ): string {
+    const match = note?.match( /Paper [\d]/ ) ;
+    return match ? match[0] : '' ;
+  }
 }
